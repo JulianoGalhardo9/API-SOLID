d78d2a31aa6189a3d953cc2b34d0de0ff3130b60
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.3>
2025-11-24 19:07:17 -0300

    feat: Caso de Uso e Design Patterns - Interface do repositório

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { prisma } from '@/lib/prisma'
 import z from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
-import { InMemoryRepository } from '@/repositories/in-memory-users-repository'
+import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -14,7 +13,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    const usersRepository = new InMemoryRepository()
+    const usersRepository = new PrismaUsersRepository()
     const registerUseCase = new RegisterUseCase(usersRepository)
 
     await registerUseCase.execute({
