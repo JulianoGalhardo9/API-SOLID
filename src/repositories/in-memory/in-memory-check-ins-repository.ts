0b240b07add55dbaa19b280a1052f6f8f714e809
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.3>
2025-11-27 14:04:13 -0300

    feat: Implementando casos de uso - TDD & Mocking

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -1,9 +1,22 @@
 import { Prisma, CheckIn } from "@prisma/client";
 import { CheckInsRepository } from "../check-ins-repository";
 import { randomUUID } from "node:crypto";
+import { check } from "zod";
 
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
+
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const checkInOnSameDate = this.items.find(
+        (checkIn) => checkIn.user_id === userId,
+    )
+
+    if (!checkInOnSameDate) {
+        return null
+    }
+
+    return checkInOnSameDate
+  }
 
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkIn: CheckIn = {
--- src/use-cases/check-in.spec.ts
+++ src/use-cases/check-in.spec.ts
@@ -1,8 +1,6 @@
-import { expect, describe, it, beforeEach } from "vitest";
-import { RegisterUseCase } from "./register";
-import { InMemoryUsersRepository } from "@/repositories/in-memory/in-memory-users-repository";
+import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
-import { CheckInUseCase } from "./checkin";
+import { CheckInUseCase } from "./check-in";
 
 let checkInRepository: InMemoryCheckInsRepository;
 let sut: CheckInUseCase;
@@ -11,14 +9,42 @@ describe("Check-in Use Case", () => {
   beforeEach(() => {
     checkInRepository = new InMemoryCheckInsRepository();
     sut = new CheckInUseCase(checkInRepository);
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
   });
 
   it("should be able to check in", async () => {
     const { checkIn } = await sut.execute({
-      gymId: 'gym-01',
-      userId: 'user-01',
+      gymId: "gym-01",
+      userId: "user-01",
     });
 
+    console.log(checkIn.created_at);
+
     expect(checkIn.id).toEqual(expect.any(String));
+  });
+
+  // Red stage, green stage and refactor stage
+
+  it("should not be able to check in twice but in different days", async () => {
+    vi.setSystemTime(new Date(2022, 0, 20, 8, 0, 0));
+
+    await sut.execute({
+      gymId: "gym-01",
+      userId: "user-01",
+    });
+
+    vi.setSystemTime(new Date(2022, 0, 21, 8, 0, 0));
+
+    const { checkIn } = await sut.execute({
+        gymId: "gym-01",
+        userId: "user-01",
+      })
+
+      expect(checkIn.id).toEqual(expect.any(String))
   });
 });
