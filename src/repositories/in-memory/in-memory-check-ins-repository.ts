e5caf4427c5da99ae4488230b129620a22f791c1
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.3>
2025-12-01 00:00:00 -0300

    feat: Implementando casos de uso - Validando horário do check-in

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -3,9 +3,18 @@ import { CheckInsRepository } from "../check-ins-repository";
 import { randomUUID } from "node:crypto";
 import dayjs from "dayjs";
 
-
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
+
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
+
+    if (!checkIn) {
+      return null;
+    }
+
+    return checkIn;
+  }
 
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf("date");
@@ -15,8 +24,7 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       const checkInDate = dayjs(checkIn.created_at);
 
       const isOnSameDate =
-        checkInDate.isAfter(startOfTheDay) &&
-        checkInDate.isBefore(endOfTheDay);
+        checkInDate.isAfter(startOfTheDay) && checkInDate.isBefore(endOfTheDay);
 
       return checkIn.user_id === userId && isOnSameDate;
     });
@@ -26,13 +34,12 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
   async findManyByUserId(userId: string, page: number) {
     return this.items
-    .filter((item) => item.user_id === userId)
-    .slice((page - 1) * 20, 40)
+      .filter((item) => item.user_id === userId)
+      .slice((page - 1) * 20, 40);
   }
 
-  async countByUserId(userId: string,) {
-    return this.items
-    .filter((item) => item.user_id === userId).length
+  async countByUserId(userId: string) {
+    return this.items.filter((item) => item.user_id === userId).length;
   }
 
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
@@ -45,6 +52,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     };
 
     this.items.push(checkIn);
+
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
 
     return checkIn;
   }
---
+++ src/use-cases/validate-check-in.spec.ts
@@ -0,0 +1,62 @@
+import { describe, it, beforeEach, expect, vi } from "vitest";
+import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+import { ValidateCheckInUseCase } from "./validate-check-in";
+import { ResourceNotFoundError } from "./errors/resource-not-found-error";
+import { after, afterEach } from "node:test";
+
+let checkInsRepository: InMemoryCheckInsRepository;
+let sut: ValidateCheckInUseCase;
+
+describe("Validate Check-in Use Case", () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository();
+    sut = new ValidateCheckInUseCase(checkInsRepository);
+
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.useRealTimers();
+  });
+
+  it("should be able to validate the check-in", async () => {
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const { checkIn } = await sut.execute({
+      checkInId: createdCheckIn.id,
+    });
+
+    expect(checkIn.is_validated).toEqual(expect.any(Date));
+    expect(checkIn!.is_validated).toEqual(expect.any(Date));
+  });
+
+  it("should not be able to validate an inexistent check-in", async () => {
+    expect(() =>
+      sut.execute({
+        checkInId: "inexistent-check-in-id",
+      })
+    ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2025, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const twetyOneMinutesInMs = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twetyOneMinutesInMs);
+
+    await expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(Error)
+  });
+});
