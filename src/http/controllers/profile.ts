03bba2a4f5213e7468ef5cd2edb0ac34c8969292
Juliano Galhardo de Oliveira <julianogalhardo@192.168.1.3>
2025-12-01 21:06:58 -0300

    feat: Controllers & Testes E2E - Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,18 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyRequest, FastifyReply } from "fastify";
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   await request.jwtVerify();
 
-  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  return reply.status(200).send();
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  });
+
+  const { password_hash, ...safeUser } = user;
+
+  return reply.status(200).send({
+    user: safeUser,
+  });
 }
